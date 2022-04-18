44f7fee9451f5d0bc8c0ac376366e84d6edc8520
Ursula Montag <montagu666@gmail.com>
2022-04-18 16:35:03 +0200

    First profile structure and img files

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { StyledProfile } from "./Profile.styled";
+
+export default function Profile() {
+  return (
+    <article>
+      <figure>
+        <img src="../img/profile-pic.png" alt="" />
+      </figure>
+    </article>
+  );
+}
